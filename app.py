2b2fb45a2a6e6d53290763f0513ee7c0abafd6f9
Hasnat-code <hasnatimtiaz949@gmail.com>
2026-05-05 23:52:56 +0500

    Initial commit - Stock Prediction LSTM Project

---
+++ app.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import yfinance as yf
+import tensorflow as tf
+
+print("Pandas Version:", pd.__version__)
+print("TensorFlow Version:", tf.__version__)
+print("Environment Ready!")
